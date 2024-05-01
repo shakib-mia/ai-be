f4d61c482f10d365c61ffe3cfa9c98a81d7c7415
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-05-01 13:18:07 +0600

    gitignore added

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+// Multer Configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "upload/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+const upload = multer({ storage: storage });
+// Express Routes
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.use("/upload", express.static("upload"));
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  res.send("http://localhost:5000/upload/" + req.file.filename);
+});
+// Start the server
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
